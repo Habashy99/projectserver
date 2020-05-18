319842186e933277a0daff7d1d451c774706ad0c
abdelrhman habashy <habashyahmed17@yahoo.com>
2020-05-18 14:48:47 +0200

    add type

--- api/challenge/challenge.service.js
+++ api/challenge/challenge.service.js
@@ -2,8 +2,15 @@ let pool = require("../../config/databse");
 module.exports = {
   create: (data, callBack) => {
     pool.query(
-      `insert into challenge(userid, questionid, skillid, score, timestamp) values(?,?,?,?,?)`,
-      [data.userid, data.questionid, data.skillid, data.score, data.timestamp],
+      `insert into challenge(userid, questionid, skillid, score, timestamp, type) values(?,?,?,?,?,?)`,
+      [
+        data.userid,
+        data.questionid,
+        data.skillid,
+        data.score,
+        data.timestamp,
+        type,
+      ],
       (error, results, fields) => {
         if (error) {
           return callBack(error);
@@ -11,5 +18,5 @@ module.exports = {
         return callBack(null, results);
       }
     );
-  }
+  },
 };
